c440d8ac06d655ee98b3e21042124da2661d8cf5
Kaare Jenssen <kaare.jenssen@gmail.com>
2015-11-04 03:29:00 +0100

    archisogen-1
    The first version of a archiso config folder system

---
+++ archisocreate.c
@@ -0,0 +1,24 @@
+/* adds /root/archisoconfig folder and copies normally edited files and folders */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	system("sudo pacman -S archiso");
+
+	system("sudo mkdir /root/archisoconfig");
+	system("sudo cp /usr/share/archiso/config/releng/packages.both /root/archisoconfig");
+	system("sudo cp /usr/share/archiso/config/releng/packages.x86_64 /root/archisoconfig");
+	system("sudo cp /usr/share/archiso/config/releng/packages.i686 /root/archisoconfig");
+	system("sudo cp /usr/share/archiso/config/releng/pacman.conf");
+	system("sudo mkdir /root/archisoconfig/airootfs/home");
+	system("sudo mkdir /root/archisoconfig/airootfs/root");
+	system("sudo cp /usr/share/archiso/config/releng/airootfs/root/customize_airootfs.sh /root/archisoconfig/airootfs/root/customize_airootfs.sh");
+	system("sudo mkdir /root/archisoconfig/airootfs/etc");
+
+	printf("\n\nCreated base configuration files in /root/archisoconfig - remember to always edit these files as root!\n");
+	printf("When you are satisfied with the content of archisoconfig, move to /root/ and run archisogen <destination> to create a build folder.\n");
+	printf("\nThen enter <destination> folder, and run ./build -v to create the actual ISO file. The ISO will be located in <destination>/out/ISO-file.iso\n");
+
+	return 0;
+}
---
+++ archisogen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	/* error handling */
+	if (argc < 2) {
+		printf("\nUSAGE: archisogen <destination>\n\nhttps://github.com/kaarejens/archisogen for more detailed information.\n");
+		return 1;
+	}
+	
+	char dummy[100];
+	char toodummy[100];
+	char precmd[] = "/usr/share/archiso/configs/releng ";
+
+	/* copy base files */
+	sprintf(dummy, "cp -r %s %s", precmd, argv[1]);
+	system(dummy);
+	
+	/* copy your custom files into base files */
+	sprintf(toodummy, "cp -r ~/archisoconfig/* %s", argv[1]);
+	system(toodummy);	
+
+	printf("\n\nDone.\n");
+	return 0;
+	
+}

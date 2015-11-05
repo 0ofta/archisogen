ec763065adb6fe5ebf887ab5bb72792a3107a4ad
Kaare Jenssen <kaare.jenssen@gmail.com>
2015-11-05 11:02:34 +0100

    fgd
    fdg

--- archisocreate.c
+++ archisocreate.c
@@ -4,29 +4,18 @@
 
 int main()
 {
-	system("sudo pacman -Syu");
-	system("sudo pacman -S archiso");
+	system("sudo pacman -Syu --noconfirm");
+	system("sudo pacman -S archiso --noconfirm");
 
-	system("sudo su");
+	system("sudo mkdir -p /root/archisoconfig/airootfs/root");
 
-	system("mkdir /root/archisoconfig");
-	system("cp /usr/share/archiso/configs/releng/packages.both /root/archisoconfig");
-	system("cp /usr/share/archiso/configs/releng/packages.x86_64 /root/archisoconfig");
-	system("cp /usr/share/archiso/configs/releng/packages.i686 /root/archisoconfig");
-	system("cp /usr/share/archiso/configs/releng/pacman.conf");
+	system("sudo cp /usr/share/archiso/configs/releng/packages.* /root/archisoconfig");
+	system("sudo cp /usr/share/archiso/configs/releng/pacman.conf");
+	system("sudo cp /usr/share/archiso/configs/releng/airootfs/root/customize_airootfs.sh /root/archisoconfig/airootfs/root/customize_airootfs.sh");
 	
-	system("mkdir /root/archisoconfig/airootfs");
-	system("mkdir /root/archisoconfig/airootfs/home");
-	
-	system("mkdir /root/archisoconfig/airootfs/root");
-	system("cp /usr/share/archiso/configs/releng/airootfs/root/customize_airootfs.sh /root/archisoconfig/airootfs/root/customize_airootfs.sh");
-	
-	system("mkdir /root/archisoconfig/airootfs/etc");
+	system("sudo chmod +x archisogen");
+	system("sudo cp archisogen /usr/bin");
 
-	system("chmod +x archisogen");
-	system("cp archisogen /usr/bin");
-
-	system("clear");
 	printf("\n\nCreated base configuration files in /root/archisoconfig - remember to always edit these files as root!\n");
 	printf("When you are satisfied with the content of archisoconfig, move to /root/ and run archisogen <destination> to create a build folder.\n");
 	printf("\nThen enter <destination> folder, and run ./build -v to create the actual ISO file. The ISO will be located in <destination>/out/ISO-file.iso\n");

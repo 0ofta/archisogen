e3aeff0ec5ab5330961d835ba941dd7d376d11e7
Kaare Jenssen <kaarejens@users.noreply.github.com>
2015-11-05 10:12:17 +0000

    Update archisocreate.c

--- archisocreate.c
+++ archisocreate.c
@@ -18,7 +18,7 @@ int main()
 
 	printf("\n\nCreated base configuration files in /root/archisoconfig - remember to always edit these files as root!\n");
 	printf("When you are satisfied with the content of archisoconfig, move to /root/ and run archisogen <destination> to create a build folder.\n");
-	printf("\nThen enter <destination> folder, and run ./build -v to create the actual ISO file. The ISO will be located in <destination>/out/ISO-file.iso\n");
+	printf("\nThen enter <destination> folder, and run ./build.sh -v to create the actual ISO file. The ISO will be located in <destination>/out/ISO-file.iso\n");
 
 	return 0;
 }

b1d1bf30c6d20ca6f66547086e50964fb251bd80
Kaare Jenssen <kaare@jenssen.it>
2019-12-31 16:01:14 +0100

    Added lisence and updated archisogen.c

--- archisogen.c
+++ archisogen.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <errno.h>
 
 int main(int argc, char **argv)
 {
@@ -14,14 +16,27 @@ int main(int argc, char **argv)
 	char precmd[] = "/usr/share/archiso/configs/releng ";
 
 	/* copy base files */
+	printf("\nCopying base files...");
 	sprintf(dummy, "cp -r %s %s", precmd, argv[1]);
 	system(dummy);
 	
-	/* copy your custom files into base files */
+	/* copy custom files into base files */
+	printf("\nCopying custom files to base...");
 	sprintf(toodummy, "cp -r /root/archlive/* %s", argv[1]);
 	system(toodummy);	
 
-	printf("\n\nDone. Go to argv[1] to build ISO file.\n");
+	/* Check if new directory exist */
+	DIR* dir = opendir(argv[1]);
+	if (dir) {
+		/* directory exist */
+		printf("\nMaking build folder [SUCCESS]\n\nGo to %s and run ./build.sh -v to build your ISO file.", argv[1]);
+		closedir(dir);
+	} else if (ENOENT == errno) {
+		/* directory does not exist */
+		printf("\nMaking build folder [FAILED]\n");
+	} else {
+		/* opendir() failed for some reason */
+	printf("Unknown error - could not check build status.\nPlease Check if your folder was created/populated.\nThis proparby means your build folder was not created successfully!\n");
+	}
 	return 0;
-	
 }
